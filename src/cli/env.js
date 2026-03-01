2c77ead671888dd580b735ffd2254400d143f0fd
Madina <madina.m.9509@gmail.com>
2026-03-01 13:31:15 +0500

    feat: implement functions command line interface

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-  // Write your code here
+  const envVars = process.env;
+
+  const result = Object.entries(envVars)
+    .filter(([key]) => key.startsWith('RSS_'))
+    .map(([key, value]) => `${key}=${value}`)
+    .join('; ');
+
+  console.log(result);
 };
 
 parseEnv();
