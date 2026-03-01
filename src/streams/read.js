9a8ac3bda266c0ae7ca4e64a0d7652d4c1002c50
Madina <madina.m.9509@gmail.com>
2026-03-01 14:57:18 +0500

    feat: implement Streams read, write, transform with proper piping and stdout/stdin handlin

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import { createReadStream } from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-  // Write your code here
+  const filePath = path.resolve(__dirname, 'files', 'fileToRead.txt');
+
+  const readable = createReadStream(filePath);
+
+  readable.on('error', () => {
+    throw new Error('FS operation failed');
+  });
+
+  readable.pipe(process.stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { createWriteStream } from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-  // Write your code here
+  const filePath = path.resolve(__dirname, 'files', 'fileToWrite.txt');
+
+  const writable = createWriteStream(filePath);
+
+  process.stdin.pipe(writable);
 };
 
 await write();
